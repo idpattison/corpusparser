034ce2a12d46cfb55c5f52dc43e51ed02bc33a79
Ian Pattison <ianpattison2@gmail.com>
2024-02-12 20:40:41 +0000

    add standard form conversion

--- sandbox.py
+++ sandbox.py
@@ -3,11 +3,9 @@ from src.corpusparser import Document
 
 filename = 'tests/data/input.xml'
 d = Document()
-breakpoint()
 d.import_colmep_format(filename, 'Dummy name')
-breakpoint()
 print(d.tag)
 # print the first part of the XML to check
 ET.indent(d, space='  ')
 xml = ET.tostring(d, encoding='unicode')
-print(xml)
+print(xml[:1000])
--- src/corpusparser.py
+++ src/corpusparser.py
@@ -9,11 +9,20 @@ class Document(ET.Element):
     def __init__(self) -> None:
         super().__init__('document')
 
-    def import_colmep_format(self, filename: str, name: str) -> None:
-
+    def import_colmep_format(self, filename: str, name: str, basic=False) -> None:
         # ensure the document is empty
         self.clear()
 
+        # import the file into basic XML format and name it
+        self.import_colmep_to_basic(filename)
+        self.set('name', name)
+
+        # if required, comvert from basic format to standard
+        if not basic:
+            self.convert_basic_to_standard()
+
+
+    def import_colmep_to_basic(self, filename: str) -> None:
         # parse the file into a temporary ElementTree and iterate across it
         tmp_tree = ET.parse(filename)
 
@@ -33,11 +42,48 @@ class Document(ET.Element):
                         text_elem.text = elem.tail
 
 
+    def convert_basic_to_standard(self) -> None:
+        # set up current document, sentence and word
+        w = None
+
+        # clone the document and clear the original - it will be easier to copy across than to keep track of iterators
+        d_old = cloneElement(self)
+        self.clear()
+        self.set('name', d_old.get('name'))
+
+        # iterate across the existing document
+        for elem in d_old.iter():
+                
+            # if this is a text element
+            if elem.tag == 'text':
+
+                # split the text into tokens based on whitespace
+                tokens = elem.text.split()
+
+                # iterate across each token
+                for token in tokens:
+                    # add the token to the document as a w element
+                    # NB this will include punctuation
+                    w = ET.SubElement(self, 'w')
+                    w.text = token
+
+            # if this is a page, comment or footnote tag, copy it into the document
+            if elem.tag in ['newpage', 'newfolio', 'comment', 'footnote']:
+                tag = ET.SubElement(self, elem.tag)
+                tag.text = elem.text
+                attribs = elem.attrib.items()
+                tag.attrib.update(attribs)
+        
+
+
 class Sentence(ET.Element):
     pass
 
 class Word(ET.Element):
     pass
 
-
-
+# create a new deep copy of any element in the tree
+def cloneElement(element: ET.Element) -> ET.Element:
+    # the easiest way is to convert to an XML string then parse back out
+    xml = ET.tostring(element, encoding='unicode')
+    return ET.fromstring(xml)
