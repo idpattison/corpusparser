98b16cde2edc5071c3d6f1a22373d8d5a3f535da
Ian Pattison <ianpattison2@gmail.com>
2024-07-12 11:48:48 +0100

    tidy up unused code

--- sandbox2.py
+++ sandbox2.py
@@ -5,9 +5,12 @@ from corpusparser import Document
 
 filename = 'tests/data/input.xml'
 format = 'colmep'
+spellings = 'tests/data/spellings.json'
+
 d = Document.create_from_nonstandard_file(filename, format)
 d.transform_tokenise_sentences()
+d.update_spellings_from_file(spellings)
 d.transform_add_convenience_text_to_sentences()
-d.transform_parse(add_parse_string=True, restructure=True)
+# d.transform_parse(add_parse_string=True, restructure=True)
 
 d.to_xml_file('tests/data/sandbox-output.xml', indent=2)
